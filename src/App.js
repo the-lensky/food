7c178b8538adf7ad3c3d9acb8b6cdd8803ef0724
the-lensky <55101239+the-lensky@users.noreply.github.com>
2021-05-08 16:50:02 +0300

    -add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+
 
 function App() {
     return (
         <>
-            <Header/>
-            <main className='content content'>
-
-            </main>
-            <Footer/>
+            <Router>
+                <Header/>
+                <main className='content content'>
+                    <Switch>
+                        <Route path='/' exact component={Home}/>
+                        <Route path='/contact' component={Contact}/>
+                        <Route path='/about' component={About}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </main>
+                <Footer/>
+            </Router>
         </>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const Header = () => {
     return (
         <nav className='green'>
             <div className="nav-wrapper">
-                <a href="/" className="brand-logo">REACT MOVIES</a>
+                <Link to="/" className="brand-logo">REACT MOVIES</Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="https://github.com/the-lensky/food" rel="noreferrer" target="_blank">Repo</a></li>
+                    <li><Link to="/contact">CONTACT</Link></li>
+                    <li><Link to="/about">ABOUT</Link></li>
                 </ul>
             </div>
         </nav>
