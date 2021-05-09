bc78eb1fa41a670d980f0c51aaa46c39c21ed562
Nick <studyaga@mail.ru>
2021-05-09 17:05:08 +0300

    -add gh-pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { Recipe } from './pages/Recipe';
 function App() {
     return (
         <>
-            <Router basename='/react-food'>
+            <Router basename='/food'>
                 <Header />
                 <main className='container content'>
                     <Switch>
